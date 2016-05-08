4c441d2cf4f8428fd569818f71954cec2affae12
SAP <shevtsov.ap@gmail.com>
2016-05-09 00:34:37 +0300

    AppDelegate format

--- TaskNavigator/AppDelegate.h
+++ TaskNavigator/AppDelegate.h
@@ -9,8 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
 
 
 @end
