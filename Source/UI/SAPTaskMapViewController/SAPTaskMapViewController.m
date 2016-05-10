6bf68dac9c49bfd2a493e802f68489c76543bfa6
SAP <shevtsov.ap@gmail.com>
2016-05-10 23:14:55 +0300

    SAPTaskMapViewController model setter override, finishModelSetting

--- Source/UI/SAPTaskMapViewController/SAPTaskMapViewController.m
+++ Source/UI/SAPTaskMapViewController/SAPTaskMapViewController.m
@@ -8,11 +8,44 @@
 
 #import "SAPTaskMapViewController.h"
 
+#import "SAPTaskMapView.h"
+#import "SAPTask.h"
+
+#import "SAPViewControllerMacro.h"
+
+SAPViewControllerBaseViewProperty(SAPTaskMapViewController, SAPTaskMapView, mainView);
+
 @interface SAPTaskMapViewController ()
 
 @end
 
 @implementation SAPTaskMapViewController
 
+@synthesize model = _model;
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setModel:(SAPModel *)model {
+    if (_model != model) {
+        _model = model;
+    }
+    
+    [self finishModelSetting];
+}
+
+#pragma mark -
+#pragma mark Public
+
+- (void)finishModelSetting {
+    SAPTask *model = self.model;
+    if (!model) {
+        return;
+    }
+    
+    CLLocationCoordinate2D coordinate = model.coordinate;
+    if (CLLocationCoordinate2DIsValid(coordinate)) {
+        self.mainView.model = model;
+    }
+}
 
 @end
