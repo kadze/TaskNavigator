19a2454b6b3b40aa9c804b0423a6d31ac56a8932
SAP <shevtsov.ap@gmail.com>
2016-05-11 18:10:35 +0300

    SAPTask.h remove redundant import

--- Source/Models/SAPTask/SAPTask.h
+++ Source/Models/SAPTask/SAPTask.h
@@ -5,8 +5,6 @@
 //  Created by Andrey on 5/9/16.
 //  Copyright © 2016 Andrey. All rights reserved.
 //
-
-#import <Foundation/Foundation.h>
 #import <CoreLocation/CLLocation.h>
 #import <MapKit/MKAnnotation.h>
 
